ba4415f24b692c0bdd8658de44280ab60b4fb0e5
jamesbeedy <jamesbeedy@gmail.com>
2016-10-22 22:23:58 +0000

    update to use new layers

--- reactive/fiche.py
+++ reactive/fiche.py
@@ -1,138 +1,143 @@
-#!/usr/bin/python3
-# Copyright (c) 2016, James Beedy <jamesbeedy@gmail.com>
-
 import os
-import sys
-import pwd
-import shutil
 import subprocess
 
-from charms.reactive import when
-from charms.reactive import when_not
-from charms.reactive import only_once
-from charms.reactive import set_state
+from charms.reactive import (
+    when,
+    when_not,
+    only_once,
+    set_state,
+    remove_state
+)
 from charmhelpers.core.templating import render
-from charmhelpers.core import hookenv
-from charmhelpers.core import host
+
+from charmhelpers.core.hookenv import (
+    config,
+    status_set,
+    close_port,
+    open_port,
+    unit_public_ip
+)
+
 from charmhelpers.core.host import chdir
 
 from nginxlib import configure_site
 
-
-FICHE_GIT_REPO = 'https://github.com/solusipse/fiche.git'
-FICHE_CODE_DIR = '/srv/fiche/'
-FICHE_SUPERVISOR_CONF = '/etc/supervisor/conf.d/fiche.conf'
-
-config = hookenv.config()
-
-FICHE_SUPERVISOR_CTXT = {
-    'fiche_server_address': hookenv.unit_public_ip(),
-    'fiche_server_port': config['fiche-server-port'],
-    'slug_size': config['slug-size'],
-    'buffer_size': config['buffer-size']
-}
+from charms.layer import options
 
 
-def _render_fiche_supervisor_conf(ctxt):
-
-    """ Render /etc/supervisor/conf.d/fiche.conf
-        and restart supervisor process.
-    """
-    if os.path.exists(FICHE_SUPERVISOR_CONF):
-        subprocess.call('supervisorctl stop fiche'.split(), shell=False)
-        os.remove(FICHE_SUPERVISOR_CONF)
-
-    render(source='fiche.conf',
-           target=FICHE_SUPERVISOR_CONF,
-           owner='root',
-           perms=0o644,
-           context=ctxt)
-
-    # Reread supervisor .conf and start/restart process
-    subprocess.call('supervisorctl reread'.split(), shell=False)
-    subprocess.call('supervisorctl update'.split(), shell=False)
-    subprocess.call('supervisorctl start fiche'.split(), shell=False)
-
-    # Restart NGINX
-    host.service_restart('nginx')
-
-
-def _ensure_basedir():
-
-    """ Ensure fiche basedir exists
-    """
-    if not os.path.isdir(FICHE_CODE_DIR):
-        os.makedirs(FICHE_CODE_DIR)
-
-    uid = pwd.getpwnam('www-data').pw_uid
-    os.chown(FICHE_CODE_DIR, uid, -1)
-
-
-@when('nginx.available')
-@when_not('fiche.available')
+@when('codebase.available')
+@when_not('fiche.installed')
 @only_once
 def install_fiche():
 
     """ Install Fiche
     """
 
-    hookenv.status_set('maintenance', 'Installing and configuring Fiche.')
+    status_set('maintenance', 'Installing and configuring Fiche.')
 
-    # Clone fiche repo
-    hookenv.status_set('maintenance', 
-                       'Installing and building fiche from github.')
-
-    git_clone_cmd = 'git clone %s /tmp/fiche' % FICHE_GIT_REPO
-    subprocess.call(git_clone_cmd.split(), shell=False)  
-  
     # Build fiche
-    with chdir("/tmp/fiche"):
+    with chdir(options('git-deploy').get('target')):
         subprocess.call('make', shell=False)
         subprocess.call('make install'.split(), shell=False)
+        subprocess.call('rm -rf /srv/fiche/*'.split(), shell=False)
 
-    # Ensure base dir exists
-    _ensure_basedir()
+    set_state('fiche.installed')
+
+
+@when('nginx.available')
+@when_not('fiche.web.configured')
+def render_nginx_conf():
 
     # Configure nginx vhost
-    configure_site('fiche', 'fiche.vhost', app_path=FICHE_CODE_DIR)
-
-    # Render fiche supervisor conf
-    _render_fiche_supervisor_conf(FICHE_SUPERVISOR_CTXT)
-    
-    # Open fiche server port
-    hookenv.open_port(config['fiche-server-port'])
-
+    configure_site('fiche', 'fiche.nginx.tmpl', port=config('port'),
+                   app_path=options('git-deploy').get('target'))
     # Open fiche front-end port
-    hookenv.open_port(config['port'])
-
-    # Set status
-    hookenv.status_set('active', 'Fiche is active on port %s' % \
-                       config['fiche-server-port'])
+    open_port(config('port'))
     # Set state
+    set_state('fiche.web.configured')
+
+
+@when('fiche.installed')
+@when_not('fiche.systemd.configured')
+def render_systemd_conf():
+    """Render fiche systemd conf
+    """
+
+    if config('fqdn'):
+        server_name = config('fqdn')
+    else:
+        server_name = unit_public_ip()
+
+    # Systemd vars
+    SYSTEMD_CTXT = {
+        'fiche_server_address': server_name,
+        'fiche_server_port': config('fiche-server-port'),
+        'slug_size': config('slug-size'),
+        'buffer_size': config('buffer-size')
+    }
+
+    if os.path.exists('/etc/systemd/system/fiche.service'):
+        os.remove('/etc/systemd/system/fiche.service')
+
+    # Render systemd template
+    render(source="fiche.service.tmpl",
+           target="/etc/systemd/system/fiche.service",
+           perms=0o644,
+           owner="root",
+           context=SYSTEMD_CTXT)
+
+    # Open fiche server port
+    open_port(config('fiche-server-port'))
+
+    # Set 'fiche.systemd.configured'
+    set_state('fiche.systemd.configured')
+
+
+@when('fiche.web.configured', 'fiche.systemd.configured',
+      'fiche.installed')
+@when_not('fiche.available')
+def fiche_available():
+    # Set status
+    status_set('active', 'Fiche is active at: %s' %
+               ("%s/%s" % (unit_public_ip(), config('fiche-server-port'))))
     set_state('fiche.available')
-    
+
 
 @when('nginx.available', 'website.available')
 def configure_website(website):
-    website.configure(port=config['port'])
+    website.configure(port=config('port'))
 
 
 @when('config.changed.fiche-server-port')
-def fiche_port_changed():
+def fiche_server_port_changed():
 
     """ React to fiche-server-port changed
     """
-    hookenv.status_set('maintenance', 'Reconfiguring fiche-server-port')
+    status_set('maintenance', 'Reconfiguring fiche-server-port')
 
+    conf = config()
     # Check and change open port, close prev port
-    if config.previous('fiche-server-port') and \
-       config.previous('fiche-server-port') != config['fiche-server-port']:
-        hookenv.close_port(config.previous('fiche-server-port'))
-        hookenv.open_port(config['fiche-server-port'])
+    if conf.previous('fiche-server-port') and \
+       conf.previous('fiche-server-port') != config('fiche-server-port'):
+        close_port(config.previous('fiche-server-port'))
+    # Remove state to re-render systemd conf
+    remove_state('fiche.systemd.configured')
+    remove_state('fiche.available')
 
-    _render_fiche_supervisor_conf(FICHE_SUPERVISOR_CTXT)
-    hookenv.status_set('active', 'Fiche is active on port %s' % \
-                       config['fiche-server-port'])
+
+@when('config.changed.port')
+def fiche_port_changed():
+
+    """ React to fiche front end port changed
+    """
+    status_set('maintenance', 'Reconfiguring fiche front end port')
+
+    conf = config()
+    # Close prev port
+    close_port(conf.previous('fiche-server-port'))
+    # Remove state to re-render nginx conf
+    remove_state('fiche.web.configured')
+    remove_state('fiche.available')
 
 
 @when('config.changed.slug-size')
@@ -140,23 +145,18 @@ def fiche_slug_size_changed():
 
     """ React to slug-size changed
     """
-    hookenv.status_set('maintenance', 'Reconfiguring slug-size')
+    status_set('maintenance', 'Reconfiguring slug-size')
+    # Remove state to re-render systemd conf
+    remove_state('fiche.systemd.configured')
+    remove_state('fiche.available')
 
-    # Rerender supervisord conf
-    _render_fiche_supervisor_conf(FICHE_SUPERVISOR_CTXT)
-
-    hookenv.status_set('active', 'Fiche is active on port %s' % \
-                       config['fiche-server-port'])
 
 @when('config.changed.buffer-size')
 def fiche_buffer_size_changed():
 
     """ React to buffer-size changed
     """
-    hookenv.status_set('maintenance', 'Reconfiguring buffer-size')
-
-    # Rerender supervisord conf
-    _render_fiche_supervisor_conf(FICHE_SUPERVISOR_CTXT)
-
-    hookenv.status_set('active', 'Fiche is active on port %s' % \
-                       config['fiche-server-port'])
+    status_set('maintenance', 'Reconfiguring buffer-size')
+    # Remove state to re-render systemd conf
+    remove_state('fiche.systemd.configured')
+    remove_state('fiche.available')
